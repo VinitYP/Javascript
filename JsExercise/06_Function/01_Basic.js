c76069af5ac46081ba8cbfe0d20f14568f41357a
Vinit Pardhi <91457234+VinitYP@users.noreply.github.com>
2023-03-08 00:29:03 +0530

    Add files via upload

---
+++ JsExercise/06_Function/01_Basic.js
@@ -0,0 +1,27 @@
+// function in javaScript
+
+// Simple :
+
+function Hello() {
+    console.log("Hello World");
+}
+Hello();
+
+// Parameter Function :
+function Country(name) {
+    console.log("I am from, "+name);
+}
+
+Country("India");
+
+// Return Function :
+function Add(num1,num2){
+    return (num1+num2);
+}
+
+var sum = Add(60,6);
+console.log("Addtion of two number is :"+sum);
+
+//or it may be like this sometime 
+
+console.log("Addtion of two number is :"+Add(10,1));
